8472ef5b7aa84de41940bdef2c8f5cb6e89225d4
Simonesmadsen <117665380+Simonesmadsen@users.noreply.github.com>
2022-11-07 16:37:53 +0100

    De første filer til MakeCode-projekt

---
+++ main.ts
@@ -0,0 +1,29 @@
+let værktøj = 0
+input.onGesture(Gesture.Shake, function () {
+    værktøj = randint(0, 2)
+    if (værktøj == 0) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (værktøj == 1) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
